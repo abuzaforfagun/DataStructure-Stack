a33e02870a98e700912007d8a781bb223a00e4d9
Abu Zafor Fagun <mdabuzaforfagun@gmail.com>
2017-12-02 19:41:35 +0600

    Implement stack in C#

--- DataStructure-Stack/Program.cs
+++ DataStructure-Stack/Program.cs
@@ -10,6 +10,39 @@ namespace DataStructure_Stack
     {
         static void Main(string[] args)
         {
+            Stack<object> stack = new Stack<object>();
+            stack.Push(20);
+            stack.Push(2);
+            stack.Push(30);
+            stack.Push("a string");
+            stack.Push("another string");
+            Console.WriteLine("Items in stack: {0}", stack.Count);
+            foreach(var item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("POP");
+            Console.WriteLine(stack.Pop().ToString());
+            Console.WriteLine("");
+            Console.WriteLine("After POP");
+            Console.WriteLine("Items in stack: {0}", stack.Count);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Peek");
+            Console.WriteLine(stack.Peek().ToString());
+            Console.WriteLine("");
+            Console.WriteLine("After Peek");
+            Console.WriteLine("Items in stack: {0}", stack.Count);
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+            Console.Read();
         }
     }
 }
